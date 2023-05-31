9e3df2970dd2f6cc88c499dc0ca27b5e0866c4a6
t-moryhassid <mory9870@gmail.com>
2023-05-31 20:28:09 +0300

    Adding a disc

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ if __name__ == '__main__':
     my_racket = pygame.Rect(1070, 220, 10, 60)
     player_1 = pygame.draw.rect(screen, color, my_racket)
     player_2 = pygame.draw.rect(screen, color, pygame.Rect(0, 220, 10, 60))
-
     dt = 0
+
+    ball_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    disc = pygame.draw.circle(screen, (255, 0, 0), ball_position, 15)
 
     while running:
 
@@ -40,6 +42,10 @@ if __name__ == '__main__':
             screen.fill(BACKGROUND_COLOR)
             my_racket.move_ip(0, 5)
             pygame.draw.rect(screen, color, my_racket)
+
+        # TODO: Add keypress listener for the keys: "w" and "s"
+        # "w" - for up
+        # "s" - for down
 
         # flip() the display to put your work on screen
         pygame.display.flip()
