f153fada4048eceaecd42d66cfa0c49a7694ad5e
t-moryhassid <mory9870@gmail.com>
2023-06-04 18:11:11 +0300

    Fixing the bug

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ if __name__ == '__main__':
     running = True
     color = (25, 0, 0)
     # Rect(left, top, width, height)
-    my_racket = pygame.Rect(1070, 220, 10, 60)
-    player_1 = pygame.draw.rect(screen, color, my_racket)
-    player_2 = pygame.draw.rect(screen, color, pygame.Rect(0, 220, 10, 60))
+    my_racket1 = pygame.Rect(1070, 220, 10, 60)
+    my_racket2 = pygame.Rect(0, 220, 10, 60)
+    player_1 = pygame.draw.rect(screen, color, my_racket1)
+    player_2 = pygame.draw.rect(screen, color, my_racket2)
     dt = 0
 
     ball_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
@@ -24,7 +25,7 @@ if __name__ == '__main__':
 
     while running:
 
-        player_1 = pygame.draw.rect(screen, color, my_racket)
+        player_1 = pygame.draw.rect(screen, color, my_racket1)
         for event in pygame.event.get():
             # here we are checking if the user wants to exit the game
             if event.type == pygame.QUIT:
@@ -32,27 +33,28 @@ if __name__ == '__main__':
                 running = False
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP()]:
+        if keys[pygame.K_UP]:
             print('Up was pressed')
             screen.fill(BACKGROUND_COLOR)
-            my_racket.move_ip(0, -5)
-            pygame.draw.rect(screen, color, my_racket)
-        if keys[pygame.K_DOWN()]:
+            my_racket1.move_ip(0, -5)
+            pygame.draw.rect(screen, color, my_racket1)
+        elif keys[pygame.K_DOWN]:
             print('Down was pressed')
             screen.fill(BACKGROUND_COLOR)
-            my_racket.move_ip(0, 5)
-            pygame.draw.rect(screen, color, my_racket)
+            my_racket1.move_ip(0, 5)
+            pygame.draw.rect(screen, color, my_racket1)
+
             keys = pygame.key.get_pressed()
-        if keys[pygame.W_UP()]:
+        if keys[pygame.K_w]:
             print('Up was pressed')
             screen.fill(BACKGROUND_COLOR)
-            my_racket.move_ip(0, -5)
-            pygame.draw.rect(screen, color, my_racket)
-        if keys[pygame.S_DOWN()]:
+            my_racket2.move_ip(0, -5)
+            pygame.draw.rect(screen, color, my_racket2)
+        elif keys[pygame.K_s]:
             print('Down was pressed')
             screen.fill(BACKGROUND_COLOR)
-            my_racket.move_ip(0, 5)
-            pygame.draw.rect(screen, color, my_racket)
+            my_racket2.move_ip(0, 5)
+            pygame.draw.rect(screen, color, my_racket2)
 
             # TODO: Add keypress listener for the keys: "w" and "s"
         # "w" - for up
