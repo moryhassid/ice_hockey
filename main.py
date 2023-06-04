7897093b5441a0dfbd1ef36af7367c5794a073bf
t-moryhassid <mory9870@gmail.com>
2023-06-04 18:32:21 +0300

    Moving ball

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import os
 
 BACKGROUND_COLOR = (0, 151, 171)
 
+
+def is_disc_hit_the_wall(ball_pos):
+    if ball_pos.x == 0 or ball_pos.x == 1070:
+        print('You have reached the left or right wall')
+    elif ball_pos.y == 0 or ball_pos.y == 440:
+        print('You have reached the upper or lower wall')
+
+
 if __name__ == '__main__':
     # pygame setup
     pygame.init()
@@ -23,7 +31,12 @@ if __name__ == '__main__':
     ball_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
     disc = pygame.draw.circle(screen, (255, 0, 0), ball_position, 15)
 
+    ball_direction = {'x': -5, 'y': 0}
+
     while running:
+        screen.fill(BACKGROUND_COLOR)
+        ball_position.x += ball_direction['x']
+        ball_position.y += ball_direction['y']
 
         player_1 = pygame.draw.rect(screen, color, my_racket1)
         for event in pygame.event.get():
@@ -35,26 +48,26 @@ if __name__ == '__main__':
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             print('Up was pressed')
-            screen.fill(BACKGROUND_COLOR)
+            # screen.fill(BACKGROUND_COLOR)
             my_racket1.move_ip(0, -5)
         elif keys[pygame.K_DOWN]:
             print('Down was pressed')
-            screen.fill(BACKGROUND_COLOR)
+            # screen.fill(BACKGROUND_COLOR)
             my_racket1.move_ip(0, 5)
             keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             print('Up was pressed')
-            screen.fill(BACKGROUND_COLOR)
             my_racket2.move_ip(0, -5)
         elif keys[pygame.K_s]:
             print('Down was pressed')
-            screen.fill(BACKGROUND_COLOR)
+            # screen.fill(BACKGROUND_COLOR)
             my_racket2.move_ip(0, 5)
 
         pygame.draw.rect(screen, color, my_racket1)
         pygame.draw.rect(screen, color, my_racket2)
         disc = pygame.draw.circle(screen, (255, 0, 0), ball_position, 15)
 
+        is_disc_hit_the_wall(ball_position)
         # flip() the display to put your work on screen
         pygame.display.flip()
 
